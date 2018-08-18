989882911c7df320ccaa85ac6633b9f00f771631
Nicholas Williams <32691042+nick-williams1@users.noreply.github.com>
2018-08-18 19:33:32 -0400

    Add files via upload

--- svm_models.R
+++ svm_models.R
@@ -7,6 +7,12 @@ setwd("C:/Users/niwi8/OneDrive/Documents/Practicum/opioid_prediction/data/cleane
 dat <- list.files(path = "C:/Users/niwi8/OneDrive/Documents/Practicum/opioid_prediction/data/cleaned/by_year", 
                   pattern = "poisoning")
 
+
+
+x <- read_sas("poisoning99.sas7bdat")
+
+x$age_cat <- as.factor(x$age_cat)
+
 list.data <- list()
 
 for (i in 1:length(dat))  {
@@ -17,4 +23,9 @@ names(list.data) <- (c("poisoning00", "poisoning01", "poisoning02", "poisoning03
                        "poisoning05", "poisoning06", "poisoning07", "poisoning08", "poisoning09", 
                        "poisoning10", "poisoning11", "poisoning12", "poisoning13", "poisoning14", 
                        "poisoning15", "poisoning16", "poisoning99"))
+
+for (i in 1:length(list.data)) {
+  list.data[[i]]$age_cat <- as.factor(list.data[[i]]$age_cat)
+}
+
 
