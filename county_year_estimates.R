c6b1bdcf59821e02f7e877134ba47aff32163113
Nicholas Williams <32691042+nick-williams1@users.noreply.github.com>
2018-08-21 14:42:51 -0400

    Add files via upload

--- county_year_estimates.R
+++ county_year_estimates.R
@@ -11,7 +11,16 @@ county_death_year <- df %>% mutate(death = 1) %>%
   group_by(county, year) %>%
   count(death) 
 
+county_opioid_year <- df %>% filter(any_opioid == 1) %>% 
+  group_by(county, year) %>% 
+  count(any_opioid)
+
 #saving data 
 
-write.csv(county_death_year, "C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/opioid_prediction/data/cleaned/county_death_year.csv", 
+write.csv(county_death_year, 
+          "C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/opioid_prediction/data/cleaned/county_death_year.csv", 
+          row.names = FALSE)
+
+write.csv(county_opioid_year, 
+          "C:/Users/niwi8/OneDrive - cumc.columbia.edu/Practicum/opioid_prediction/data/cleaned/county_opioid_year.csv",
           row.names = FALSE)
